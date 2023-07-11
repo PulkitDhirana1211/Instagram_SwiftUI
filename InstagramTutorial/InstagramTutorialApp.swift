28a20759dfe511446e0b8aac9a70a591aa6eaac9
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2023-07-12 00:55:09 +0530

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Pulkit Dhirana on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
